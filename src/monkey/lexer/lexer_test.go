caf9231f6112b8ae68912ccc6e3dfa87df0c6861
Satomin2864 <satomiyuta0602@gmail.com>
2018-11-24 20:08:36 +0900

    ディレクトリ構造の変更

---
+++ src/monkey/lexer/lexer_test.go
@@ -0,0 +1,100 @@
+package lexer
+
+import (
+  "testing"
+
+  "../token"
+)
+func TestNextToken(t *testing.T) {
+    input :=`let five = 5;
+  let ten = 10;
+  let add = fn(x, y) {
+    x + y;
+  };
+
+  let result = add(five, ten);
+  !-/*5;
+  5 < 10 > 5;
+
+  10 == 10;
+  10 != 9;
+  `
+
+  tests  := []struct {
+    expectedType token.TokenType
+    expectedLiteral string
+  }{
+    {token.LET, "let"},
+    {token.INDENT, "five"},
+    {token.ASSIGN, "="},
+    {token.INT, "5"},
+    {token.SEMICOLON, ";"},
+    {token.LET, "let"},
+    {token.INDENT, "ten"},
+    {token.ASSIGN, "="},
+    {token.INT, "10"},
+    {token.SEMICOLON, ";"},
+    {token.LET, "let"},
+    {token.INDENT, "add"},
+    {token.ASSIGN, "="},
+    {token.FUNCTION, "fn"},
+    {token.LPAREN, "("},
+    {token.INDENT, "x"},
+    {token.COMMA, ","},
+    {token.INDENT, "y"},
+    {token.RPAREN, ")"},
+    {token.LBRACE, "{"},
+    {token.INDENT, "x"},
+    {token.PLUS, "+"},
+    {token.INDENT, "y"},
+    {token.SEMICOLON, ";"},
+    {token.RBRACE, "}"},
+    {token.SEMICOLON, ";"},
+    {token.LET, "let"},
+    {token.INDENT, "result"},
+    {token.ASSIGN, "="},
+    {token.INDENT, "add"},
+    {token.LPAREN, "("},
+    {token.INDENT, "five"},
+    {token.COMMA, ","},
+    {token.INDENT, "ten"},
+    {token.RPAREN, ")"},
+    {token.SEMICOLON, ";"},
+    {token.BANG, "!"},
+    {token.MINUS, "-"},
+    {token.SLASH, "/"},
+    {token.ASTERISK, "*"},
+    {token.INT, "5"},
+    {token.SEMICOLON, ";"},
+    {token.INT, "5"},
+    {token.LT, "<"},
+    {token.INT, "10"},
+    {token.GT, ">"},
+    {token.INT, "5"},
+    {token.SEMICOLON, ";"},
+    {token.INT, "10"},
+    {token.EQ, "=="},
+    {token.INT, "10"},
+    {token.SEMICOLON, ";"},
+    {token.INT, "10"},
+    {token.NOT_EQ, "!="},
+    {token.INT, "9"},
+    {token.SEMICOLON, ";"},
+    {token.EOF, ""},
+  }
+  l := New(input)
+
+  for i, tt := range tests {
+    tok := l.NextToken()
+
+    if tok.Type != tt.expectedType {
+      t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+        i, tt.expectedType, tok.Literal)
+    }
+
+    if tok.Literal != tt.expectedLiteral {
+      t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+        i, tt.expectedLiteral, tok.Literal)
+    }
+  }
+}
